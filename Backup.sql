e5ba2ea3344acf4983514d1f8733672665d0a38c
Micalopez11 <lopezmica499@gmail.com>
2023-11-19 01:57:23 -0300

    base de datos

---
+++ Backup.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE  IF NOT EXISTS `integrador_cac` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `integrador_cac`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: integrador_cac
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `oradores`
+--
+
+DROP TABLE IF EXISTS `oradores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `oradores` (
+  `id_orador` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `mail` varchar(100) DEFAULT NULL,
+  `tema` varchar(100) DEFAULT NULL,
+  `fecha_alta` date DEFAULT NULL,
+  PRIMARY KEY (`id_orador`),
+  UNIQUE KEY `mail` (`mail`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oradores`
+--
+
+LOCK TABLES `oradores` WRITE;
+/*!40000 ALTER TABLE `oradores` DISABLE KEYS */;
+INSERT INTO `oradores` VALUES (1,'Lionel','Messi','lionel.messi@gmail.com','Futbol','1987-06-24'),(2,'Angel','Di Maria','angel.dimaria@gmail.com','Literatura','1988-02-14'),(3,'Cristian','Romero','cristian.romero@gmail.com','Boxeo','1998-04-27'),(4,'Julian','Alvarez','julian.alvarez@gmail.com','Matematicas','2000-01-31'),(5,'Lautaro','Martinez','lautaro.martinez@gmail.com','Geografia','1997-08-22'),(6,'Enzo','Fernandez','enzo.fernandez@gmail.com','Historia','2001-01-17'),(7,'Nahuel','Molina','nahuel.molina@gmail.com','Tenis','1998-04-06'),(8,'Nicolas','Tagliafico','nicolas.tagliafico@gmail.com','Filosofia','1992-08-31'),(9,'Nicolas','Otamendi','nicolas.otamendi@gmail.com','Automovilismo','1988-02-12'),(10,'Emiliano','Martinez','emiliano.martinez@gmail.com','Musica','1992-09-02');
+/*!40000 ALTER TABLE `oradores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'integrador_cac'
+--
+
+--
+-- Dumping routines for database 'integrador_cac'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-19  1:33:28
